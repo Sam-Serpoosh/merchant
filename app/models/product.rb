82de8040e438f6bcd0016b5bf1ca071a7e8f0d80
Sam Serpoosh <Sam@unknown5c969d97f1b7.att.net>
2013-03-23 22:22:54 -0400

    Added the Stock to the product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :title, :price, :description, :image_url
+  attr_accessible :title, :price, :description, :image_url, :stock
 
   validates :price, numericality: true
+  validates :stock, numericality: { only_integer: true, 
+                                    greater_than_or_equal_to: 0 }
 
   def price=(value)
     value.delete!("$")
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Product do
+  context "#validation" do
+    it "only accepts integers for stock" do
+      p = Product.new(price: "25.99", stock: 10)
+      p.should be_valid
+    end
+
+    it "rejects non-integer stock values" do
+      p = Product.new(price: "20", stock: 10.99)
+      p.should_not be_valid
+    end
+
+    it "rejects stock less than 0" do
+      p = Product.new(price: "20", stock: -10)
+      p.should_not be_valid
+    end
+  end
+end
